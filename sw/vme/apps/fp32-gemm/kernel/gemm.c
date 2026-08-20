a43a02a3601acfcf281728314c2d075613a855bf
Pei-Yu Lin <62455939+boboloiono@users.noreply.github.com>
2026-08-20 11:08:48 +0200

    [SW] update fp-gemm with cross-block

--- sw/vme/apps/fp32-gemm/kernel/gemm.c
+++ sw/vme/apps/fp32-gemm/kernel/gemm.c
@@ -65,31 +65,83 @@ __attribute__((noinline, aligned(64))) void gemm_fp32(
     uintptr_t loop_counter;
 
     asm volatile(
-        // Keep all column blocks in one call and preserve pointer state in
-        // registers across block transitions.
-        "9:\n"
-
         // K-Group 1
         // vle A0
         "msetmtype %[mtype], %[vtype]\n"
         "msettn x0, %[tn]\n"
         "vsetvli x0, %[group_vl], e32, m8, ta, ma\n"
-        "vle32.v v0,  (%[a0p])\n" "addi %[a0p], %[a0p], 512\n"
-        "vle32.v v16,  (%[b0p])\n" "addi %[b0p], %[b0p], 512\n"
+        "vle32.v v0,  (%[a0p])\n"
+        "vle32.v v16,  (%[b0p])\n"
         "msettm x0, %[tm]\n"
         "msettk x0, %[tk]\n"
         "vtzero mt0\n"
-        "vtzero mt4\n"
-        "vtzero mt12\n"
-        "vtzero mt8\n"
         
+        // vle B1
+        // vtfmm mt0
+        "vle32.v v24,  (%[b1p])\n"
+        "vtfmm.tvv mt0, v0, v16\n"
+        "addi %[a0p], %[a0p], 512\n"
+        "vtfmm.tvv mt0, v1, v17\n"
+        "addi %[b0p], %[b0p], 512\n"
+        "vtfmm.tvv mt0, v2, v18\n"
+        "addi %[b1p], %[b1p], 512\n"
+        "vtfmm.tvv mt0, v3, v19\n"
+        "vtfmm.tvv mt0, v4, v20\n"
+        "vtfmm.tvv mt0, v5, v21\n"
+        "vtzero mt4\n"
+        "vtfmm.tvv mt0, v6, v22\n"
+        "vtfmm.tvv mt0, v7, v23\n"
+
+        // vle A1
+        // vtfmm mt4
+        "vle32.v v8,  (%[a1p])\n"
+        "vtfmm.tvv mt4, v0, v24\n"
+        "addi %[a1p], %[a1p], 512\n"
+        "vtfmm.tvv mt4, v1, v25\n"
+        "vtfmm.tvv mt4, v2, v26\n"
+        "vtfmm.tvv mt4, v3, v27\n"
+        "vtfmm.tvv mt4, v4, v28\n"
+        "vtfmm.tvv mt4, v5, v29\n"
+        "vtzero mt8\n"
+        "vtfmm.tvv mt4, v6, v30\n"
+        "vtfmm.tvv mt4, v7, v31\n"
+
+        // preload vle A0
+        // vtfmm mt8
+        "vle32.v v0,  (%[a0p])\n"
+        "vtfmm.tvv mt8, v8,  v16\n"
+        "addi %[a0p], %[a0p], 512\n"
+        "vtfmm.tvv mt8, v9,  v17\n"
+        "vtfmm.tvv mt8, v10,  v18\n"
+        "vtfmm.tvv mt8, v11,  v19\n"
+        "vtfmm.tvv mt8, v12,  v20\n"
+        "vtfmm.tvv mt8, v13,  v21\n"
+        "vtzero mt12\n"
+        "vtfmm.tvv mt8, v14,  v22\n"
+        "vtfmm.tvv mt8, v15,  v23\n"
+
+        // preload vle A1
+        // vtfmm mt12
+        "vle32.v v16,  (%[b0p])\n"
+        "vtfmm.tvv mt12, v8,  v24\n"
+        "addi %[b0p], %[b0p], 512\n"
+        "vtfmm.tvv mt12, v9,  v25\n"
+        "vtfmm.tvv mt12, v10, v26\n"
+        "vtfmm.tvv mt12, v11, v27\n"
+        "vtfmm.tvv mt12, v12, v28\n"
+        "vtfmm.tvv mt12, v13, v29\n"
+        "vtfmm.tvv mt12, v14, v30\n"
+        "vtfmm.tvv mt12, v15, v31\n"
+
+        "9:\n"
         // K-Group 1 to N-2
         // vle B0
         // vtfmm mt0
-        "li %[loop], 6\n"
+        "li %[loop], 5\n"
         "1:\n"
-        "vle32.v v24,  (%[b1p])\n" "addi %[b1p], %[b1p], 512\n"
+        "vle32.v v24,  (%[b1p])\n"
         "vtfmm.tvv mt0, v0, v16\n"
+        "addi %[b1p], %[b1p], 512\n"
         "vtfmm.tvv mt0, v1, v17\n"
         "vtfmm.tvv mt0, v2, v18\n"
         "vtfmm.tvv mt0, v3, v19\n"
@@ -100,8 +152,9 @@ __attribute__((noinline, aligned(64))) void gemm_fp32(
 
         // vle B1
         // vtfmm mt4
-        "vle32.v v8,  (%[a1p])\n" "addi %[a1p], %[a1p], 512\n"
+        "vle32.v v8,  (%[a1p])\n"
         "vtfmm.tvv mt4, v0, v24\n"
+        "addi %[a1p], %[a1p], 512\n"
         "vtfmm.tvv mt4, v1, v25\n"
         "vtfmm.tvv mt4, v2, v26\n"
         "vtfmm.tvv mt4, v3, v27\n"
@@ -112,8 +165,9 @@ __attribute__((noinline, aligned(64))) void gemm_fp32(
 
         // vle A1
         // vtfmm mt8
-        "vle32.v v0,  (%[a0p])\n" "addi %[a0p], %[a0p], 512\n"
+        "vle32.v v0,  (%[a0p])\n"
         "vtfmm.tvv mt8, v8,  v16\n"
+        "addi %[a0p], %[a0p], 512\n"
         "vtfmm.tvv mt8, v9,  v17\n"
         "vtfmm.tvv mt8, v10,  v18\n"
         "vtfmm.tvv mt8, v11,  v19\n"
@@ -125,8 +179,9 @@ __attribute__((noinline, aligned(64))) void gemm_fp32(
         // preload vle A0
         // vtfmm mt12
         "addi %[loop], %[loop], -1\n"
-        "vle32.v v16,  (%[b0p])\n" "addi %[b0p], %[b0p], 512\n"
+        "vle32.v v16,  (%[b0p])\n"
         "vtfmm.tvv mt12, v8,  v24\n"
+        "addi %[b0p], %[b0p], 512\n"
         "vtfmm.tvv mt12, v9,  v25\n"
         "vtfmm.tvv mt12, v10, v26\n"
         "vtfmm.tvv mt12, v11, v27\n"
@@ -139,13 +194,15 @@ __attribute__((noinline, aligned(64))) void gemm_fp32(
         // K-Group N-1 & K-Group N
         // vle B0
         // vtfmm mt0
-        "vle32.v v8,  (%[a0p])\n" "addi %[a0p], %[a0p], 512\n"
+        "vle32.v v8,  (%[a0p])\n"
         "vtfmm.tvv mt0, v0, v16\n"
+        "addi %[a0p], %[a0p], 512\n"
         "vtfmm.tvv mt0, v1, v17\n"
         "vtfmm.tvv mt0, v2, v18\n"
         "vtfmm.tvv mt0, v3, v19\n"
-        "vle32.v v24,  (%[b0p])\n" "addi %[b0p], %[b0p], 512\n"
+        "vle32.v v24,  (%[b0p])\n"
         "vtfmm.tvv mt0, v4, v20\n"
+        "addi %[b0p], %[b0p], 512\n"
         "vtfmm.tvv mt0, v5, v21\n"
         "vtfmm.tvv mt0, v6, v22\n"
         "vtfmm.tvv mt0, v7, v23\n"
@@ -153,8 +210,9 @@ __attribute__((noinline, aligned(64))) void gemm_fp32(
         // vle A0'
         // vle B0'
         // vtfmm mt0
-        "vle32.v v16,  (%[b1p])\n" "addi %[b1p], %[b1p], 512\n"
+        "vle32.v v16,  (%[b1p])\n"
         "vtfmm.tvv mt0, v8, v24\n"
+        "addi %[b1p], %[b1p], 512\n"
         "vtfmm.tvv mt0, v9, v25\n"
         "vtfmm.tvv mt0, v10, v26\n"
         "vtfmm.tvv mt0, v11, v27\n"
@@ -165,7 +223,7 @@ __attribute__((noinline, aligned(64))) void gemm_fp32(
         
         // vle B1 (reuse A0)
         // vtfmm mt4
-        "vle32.v v24,  (%[b1p])\n" "addi %[b1p], %[b1p], 512\n"
+        "vle32.v v24,  (%[b1p])\n"
 
         "vtse32 %[tss0], (%[p00])\n"
         "add %[p00], %[p00], %[c_stride]\n"
@@ -173,6 +231,7 @@ __attribute__((noinline, aligned(64))) void gemm_fp32(
         "vtfmm.tvv mt4, v0, v16\n"
 
         "vtse32 %[tss0], (%[p00])\n"
+        "addi %[b1p], %[b1p], 512\n"
         "add %[p00], %[p00], %[c_stride]\n"
         "addi %[tss0], %[tss0], 1\n"
         "vtfmm.tvv mt4, v1, v17\n"
@@ -245,6 +304,7 @@ __attribute__((noinline, aligned(64))) void gemm_fp32(
         "vtfmm.tvv mt4, v14, v30\n"
 
         "vtse32 %[tss0], (%[p00])\n"
+        "vle32.v v0,  (%[a1p])\n"
         "add %[p00], %[p00], %[c_stride]\n"
         "addi %[tss0], %[tss0], 1\n"
         "vtfmm.tvv mt4, v15, v31\n"
@@ -252,9 +312,9 @@ __attribute__((noinline, aligned(64))) void gemm_fp32(
         // vle A1 (reuse B0)
         // vtfmm mt12
         // vtse mt4
-        "vle32.v v0,  (%[a1p])\n" "addi %[a1p], %[a1p], 512\n"
 
         "vtse32 %[tss4], (%[p01])\n"
+        "addi %[a1p], %[a1p], 512\n"
         "add %[p01], %[p01], %[c_stride]\n"
         "addi %[tss4], %[tss4], 1\n"
         "vtfmm.tvv mt12, v0,  v16\n"
@@ -290,6 +350,7 @@ __attribute__((noinline, aligned(64))) void gemm_fp32(
         "vtfmm.tvv mt12, v6,  v22\n"
 
         "vtse32 %[tss4], (%[p01])\n"
+        "vle32.v v8,  (%[a1p])\n"
         "add %[p01], %[p01], %[c_stride]\n"
         "addi %[tss4], %[tss4], 1\n"
         "vtfmm.tvv mt12, v7,  v23\n"
@@ -297,9 +358,9 @@ __attribute__((noinline, aligned(64))) void gemm_fp32(
         // vle A1' (reuse B0')
         // vtfmm mt12
         // vtse mt4
-        "vle32.v v8,  (%[a1p])\n" "addi %[a1p], %[a1p], 512\n"
 
         "vtse32 %[tss4], (%[p01])\n"
+        "addi %[a1p], %[a1p], 512\n"
         "add %[p01], %[p01], %[c_stride]\n"
         "addi %[tss4], %[tss4], 1\n"
         "vtfmm.tvv mt12, v8,  v24\n"
@@ -327,6 +388,7 @@ __attribute__((noinline, aligned(64))) void gemm_fp32(
         "vtse32 %[tss4], (%[p01])\n"
         "add %[p01], %[p01], %[c_stride]\n"
         "addi %[tss4], %[tss4], 1\n"
+        "addi %[b0p], %[b0p], -1024\n"
         "vtfmm.tvv mt12, v13,  v29\n"
 
         "vtse32 %[tss4], (%[p01])\n"
@@ -335,6 +397,7 @@ __attribute__((noinline, aligned(64))) void gemm_fp32(
         "vtfmm.tvv mt12, v14,  v30\n"
 
         "vtse32 %[tss4], (%[p01])\n"
+        "vle32.v v16,  (%[b0p])\n"
         "add %[p01], %[p01], %[c_stride]\n"
         "addi %[tss4], %[tss4], 1\n"
         "vtfmm.tvv mt12, v15,  v31\n"
@@ -342,10 +405,8 @@ __attribute__((noinline, aligned(64))) void gemm_fp32(
         // vle B0 (reuse A1)
         // vtfmm mt8
         // vtse mt12
-        "addi %[b0p], %[b0p], -1024\n"
-        "vle32.v v16,  (%[b0p])\n" "addi %[b0p], %[b0p], 512\n"
-
         "vtse32 %[tss12], (%[p11])\n"
+        "addi %[b0p], %[b0p], 512\n"
         "add %[p11], %[p11], %[c_stride]\n"
         "addi %[tss12], %[tss12], 1\n"
         "vtfmm.tvv mt8, v0,  v16\n"
@@ -381,6 +442,7 @@ __attribute__((noinline, aligned(64))) void gemm_fp32(
         "vtfmm.tvv mt8, v6,  v22\n"
 
         "vtse32 %[tss12], (%[p11])\n"
+        "vle32.v v24,  (%[b0p])\n"
         "add %[p11], %[p11], %[c_stride]\n"
         "addi %[tss12], %[tss12], 1\n"
         "vtfmm.tvv mt8, v7,  v23\n"
@@ -388,9 +450,9 @@ __attribute__((noinline, aligned(64))) void gemm_fp32(
         // vle B0' (reuse A1')
         // vtfmm mt8
         // vtse mt12
-        "vle32.v v24,  (%[b0p])\n" "addi %[b0p], %[b0p], 512\n"
 
         "vtse32 %[tss12], (%[p11])\n"
+        "addi %[b0p], %[b0p], 512\n"
         "add %[p11], %[p11], %[c_stride]\n"
         "addi %[tss12], %[tss12], 1\n"
         "vtfmm.tvv mt8, v8,  v24\n"
@@ -430,7 +492,151 @@ __attribute__((noinline, aligned(64))) void gemm_fp32(
         "addi %[tss12], %[tss12], 1\n"
         "vtfmm.tvv mt8, v15,  v31\n"
 
-        // vtse mt8
+        "addi %[blocks], %[blocks], -1\n"
+        "beqz %[blocks], 7f\n"
+
+        "sub %[loop], %[b1p], %[b0p]\n"
+        "sub %[a0p], %[a0p], %[loop]\n"
+        "vle32.v v0,  (%[a0p])\n"
+        "add %[b0p], %[b0p], %[loop]\n"
+        "vle32.v v16,  (%[b0p])\n"
+        "vtzero mt0\n"
+        "sub %[a1p], %[a1p], %[loop]\n"
+        "add %[b1p], %[b1p], %[loop]\n"
+
+        "vtse32 %[tss8], (%[p10])\n"
+        "add %[p10], %[p10], %[c_stride]\n"
+        "addi %[tss8], %[tss8], 1\n"
+        "vtfmm.tvv mt0, v0, v16\n"
+
+        "vtse32 %[tss8], (%[p10])\n"
+        "add %[p10], %[p10], %[c_stride]\n"
+        "addi %[tss8], %[tss8], 1\n"
+        "vtfmm.tvv mt0, v1, v17\n"
+
+        "vtse32 %[tss8], (%[p10])\n"
+        "add %[p10], %[p10], %[c_stride]\n"
+        "addi %[tss8], %[tss8], 1\n"
+        "vtfmm.tvv mt0, v2, v18\n"
+
+        "vtse32 %[tss8], (%[p10])\n"
+        "add %[p10], %[p10], %[c_stride]\n"
+        "addi %[tss8], %[tss8], 1\n"
+        "vle32.v v24, (%[b1p])\n"
+        "vtfmm.tvv mt0, v3, v19\n"
+
+        "vtse32 %[tss8], (%[p10])\n"
+        "add %[p10], %[p10], %[c_stride]\n"
+        "addi %[tss8], %[tss8], 1\n"
+        "vtfmm.tvv mt0, v4, v20\n"
+
+        "vtse32 %[tss8], (%[p10])\n"
+        "add %[p10], %[p10], %[c_stride]\n"
+        "addi %[tss8], %[tss8], 1\n"
+        "vtzero mt4\n"
+        "vtfmm.tvv mt0, v5, v21\n"
+
+        "vtse32 %[tss8], (%[p10])\n"
+        "add %[p10], %[p10], %[c_stride]\n"
+        "addi %[tss8], %[tss8], 1\n"
+        "vle32.v v8, (%[a1p])\n"
+        "vtfmm.tvv mt0, v6, v22\n"
+
+        "vtse32 %[tss8], (%[p10])\n"
+        "add %[p10], %[p10], %[c_stride]\n"
+        "addi %[tss8], %[tss8], 1\n"
+        "vtfmm.tvv mt0, v7, v23\n"
+
+        // Rows 8..15 overlap the next block's mt4. p10 remains the old
+        // block's store pointer until row 15 has issued.
+        "vtse32 %[tss8], (%[p10])\n"
+        "add %[p10], %[p10], %[c_stride]\n"
+        "addi %[tss8], %[tss8], 1\n"
+        "vtfmm.tvv mt4, v0, v24\n"
+
+        "vtse32 %[tss8], (%[p10])\n"
+        "add %[p10], %[p10], %[c_stride]\n"
+        "addi %[tss8], %[tss8], 1\n"
+        "vtfmm.tvv mt4, v1, v25\n"
+
+        "vtse32 %[tss8], (%[p10])\n"
+        "add %[p10], %[p10], %[c_stride]\n"
+        "addi %[tss8], %[tss8], 1\n"
+        "vtfmm.tvv mt4, v2, v26\n"
+
+        "vtse32 %[tss8], (%[p10])\n"
+        "add %[p10], %[p10], %[c_stride]\n"
+        "addi %[tss8], %[tss8], 1\n"
+        "vtfmm.tvv mt4, v3, v27\n"
+
+        "vtse32 %[tss8], (%[p10])\n"
+        "add %[p10], %[p10], %[c_stride]\n"
+        "addi %[tss8], %[tss8], 1\n"
+        "vtfmm.tvv mt4, v4, v28\n"
+
+        "vtse32 %[tss8], (%[p10])\n"
+        "add %[p10], %[p10], %[c_stride]\n"
+        "addi %[tss8], %[tss8], 1\n"
+        "vtfmm.tvv mt4, v5, v29\n"
+
+        "vtse32 %[tss8], (%[p10])\n"
+        "add %[p10], %[p10], %[c_stride]\n"
+        "addi %[tss8], %[tss8], 1\n"
+        "vtfmm.tvv mt4, v6, v30\n"
+
+        "vtse32 %[tss8], (%[p10])\n"
+        "add %[p10], %[p10], %[c_stride]\n"
+        "addi %[tss8], %[tss8], 1\n"
+        "vtfmm.tvv mt4, v7, v31\n"
+
+        // The final old mt8 row has issued, so its tile-row selector can now
+        // be reset. p10 was retargeted by the last row's spare slot above.
+
+        // preload vle A0
+        // vtfmm mt8
+        "addi %[a0p], %[a0p], 512\n"
+        "vle32.v v0,  (%[a0p])\n"
+        "vtzero mt8\n"
+        "vtfmm.tvv mt8, v8,  v16\n"
+        "addi %[a0p], %[a0p], 512\n"
+        "vtfmm.tvv mt8, v9,  v17\n"
+        "vtzero mt12\n"
+        "vtfmm.tvv mt8, v10,  v18\n"
+        "addi %[b0p], %[b0p], 512\n"
+        "vtfmm.tvv mt8, v11,  v19\n"
+        "addi %[b1p], %[b1p], 512\n"
+        "vtfmm.tvv mt8, v12,  v20\n"
+        "addi %[a1p], %[a1p], 512\n"
+        "vtfmm.tvv mt8, v13,  v21\n"
+        "vle32.v v16,  (%[b0p])\n"
+        "vtfmm.tvv mt8, v14,  v22\n"
+        "addi %[b0p], %[b0p], 512\n"
+        "vtfmm.tvv mt8, v15,  v23\n"
+
+        // preload vle A1
+        // vtfmm mt12
+        "addi %[tss0], %[tss0], -16\n"
+        "vtfmm.tvv mt12, v8,  v24\n"
+        "slli %[loop], %[c_stride], 4\n"
+        "addi %[tss4], %[tss4], -16\n"
+        "vtfmm.tvv mt12, v9,  v25\n"
+        "addi %[loop], %[loop], -128\n"
+        "addi %[tss12], %[tss12], -16\n"
+        "vtfmm.tvv mt12, v10, v26\n"
+        "addi %[tss8], %[tss8], -16\n"
+        "vtfmm.tvv mt12, v11, v27\n"
+        "sub %[p00], %[p00], %[loop]\n"
+        "vtfmm.tvv mt12, v12, v28\n"
+        "sub %[p01], %[p01], %[loop]\n"
+        "vtfmm.tvv mt12, v13, v29\n"
+        "sub %[p10], %[p10], %[loop]\n"
+        "vtfmm.tvv mt12, v14, v30\n"
+        "sub %[p11], %[p11], %[loop]\n"
+        "vtfmm.tvv mt12, v15, v31\n"
+        "j 9b\n"
+
+        // Final block: compact 16-row mt8 store loop.
+        "7:\n"
         "li %[loop], 16\n"
         "2:\n"
         "vtse32 %[tss8], (%[p10])\n"
@@ -438,31 +644,6 @@ __attribute__((noinline, aligned(64))) void gemm_fp32(
         "addi %[tss8], %[tss8], 1\n"
         "addi %[loop], %[loop], -1\n"
         "bnez %[loop], 2b\n"
-
-        "addi %[blocks], %[blocks], -1\n"
-        "beqz %[blocks], 8f\n"
-
-        // b1p and b0p have advanced equally, so their distance is one packed
-        // tile. Reuse the completed store-loop counter as a transition temp.
-        "sub %[loop], %[b1p], %[b0p]\n"
-        "sub %[a0p], %[a0p], %[loop]\n"
-        "sub %[a1p], %[a1p], %[loop]\n"
-        "add %[b0p], %[b0p], %[loop]\n"
-        "add %[b1p], %[b1p], %[loop]\n"
-
-        // Output streams are at row 16. Rewind 16 rows and advance 32 FP32
-        // columns to reach row 0 of the next column block.
-        "slli %[loop], %[c_stride], 4\n"
-        "addi %[loop], %[loop], -128\n"
-        "sub %[p00], %[p00], %[loop]\n"
-        "sub %[p01], %[p01], %[loop]\n"
-        "sub %[p10], %[p10], %[loop]\n"
-        "sub %[p11], %[p11], %[loop]\n"
-        "addi %[tss0], %[tss0], -16\n"
-        "addi %[tss4], %[tss4], -16\n"
-        "addi %[tss8], %[tss8], -16\n"
-        "addi %[tss12], %[tss12], -16\n"
-        "j 9b\n"
         "8:\n"
 
         :
